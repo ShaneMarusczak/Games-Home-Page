b3de7d5bd502b5fb72a593a8ae63ae59163be586
ShaneMarusczak <marusczaks@gmail.com>
2020-05-14 19:22:47 -0400

    initial commit

---
+++ home.js
@@ -0,0 +1,9 @@
+"use strict";
+(() => {
+
+	(() => {
+		// document.getElementById("battleship").addEventListener("click", () => {
+		// 	window.location.href = "https://marusczak.com/Battleship-JS/";
+		// });
+	})();
+})();
