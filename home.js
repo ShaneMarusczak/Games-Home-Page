e041fb4e4457b40d0a173cde22ac945be0f6b997
ShaneMarusczak <marusczaks@gmail.com>
2020-06-11 17:54:55 -0400

    dark mode

--- home.js
+++ home.js
@@ -1,7 +1,19 @@
 "use strict";
 (() => {
+	let inDarkMode = false;
+
+	const darkModeToggle = () => {
+		if (inDarkMode) {
+			document.getElementsByTagName("html")[0].classList.remove("darkBackground");
+			document.getElementById("githubicon").classList.remove("lightBackground");
+		} else {
+			document.getElementsByTagName("html")[0].classList.add("darkBackground");
+			document.getElementById("githubicon").classList.add("lightBackground");
+		}
+		inDarkMode = !inDarkMode;
+	};
 
 	(() => {
-
+		document.getElementById("darkMode").addEventListener("click", darkModeToggle);
 	})();
 })();
