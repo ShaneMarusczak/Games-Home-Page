4f5585b1807dba83c27286d9569449f65d2527b0
ShaneMarusczak <marusczaks@gmail.com>
2026-02-23 12:15:18 -0500

    no dark mode

--- js/home.js
+++ js/home.js
@@ -1,44 +1,9 @@
 "use strict";
 (() => {
-
   function sleep(ms) {
     return new Promise((resolve) => setTimeout(resolve, ms));
   }
 
-  function setCookie(cname, cvalue, exdays) {
-    var d = new Date();
-    d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-    var expires = "expires=" + d.toUTCString();
-    document.cookie =
-        cname + "=" + cvalue + ";" + expires + ";path=/;SameSite=Lax";
-  }
-
-  function getCookie(cname) {
-    var name = cname + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(";");
-    for (var i = 0; i < ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) === " ") {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) === 0) {
-        return c.substring(name.length, c.length);
-      }
-    }
-    return "";
-  }
-  function setDarkMode() {
-    document.getElementsByTagName("html")[0].classList.add("darkBackground");
-    document.getElementById("githubicon").classList.add("lightBackground");
-    setCookie("darkMode", "Y", 1);
-  }
-
-  function removeDarkMode() {
-    document.getElementsByTagName("html")[0].classList.remove("darkBackground");
-    document.getElementById("githubicon").classList.remove("lightBackground");
-    setCookie("darkMode", "N", 1);
-  }
   (() => {
     document.querySelectorAll(".linkBox").forEach((item) => {
       item.addEventListener("mouseover", () => {
@@ -52,19 +17,6 @@
       });
     });
 
-    if (getCookie("darkMode") === "Y") {
-      setDarkMode();
-    } else {
-      removeDarkMode();
-    }
-
-    document.getElementById("darkMode").addEventListener("click", () => {
-      if (getCookie("darkMode") === "N") {
-        setDarkMode();
-      } else {
-        removeDarkMode();
-      }
-    });
     document.getElementById("githubicon").addEventListener("mouseover", () => {
       document.getElementById("githubicon").classList.add("upBounce");
       document.getElementById("githubicon").classList.remove("downBounce");
